c7c1c982041a6ebde0d5f227fda89d8cd94fe089
Luis Chion <lmchion@berkeley.edu>
2023-02-14 14:18:15 -0500

    update subproc

--- examples/marl/rllib/heterogeneous/train_subproc.py
+++ examples/marl/rllib/heterogeneous/train_subproc.py
@@ -1,5 +1,10 @@
-import subprocess
+import subprocess, os
 
-list_files = subprocess.run(['python','-u','/home/lmchion/mids/w210/PowerGridworld/examples/marl/rllib/heterogeneous/train.py','--num-cpus','2','--num-gpus','0','--local-dir','./ray_results','--max-episode-steps','250'])
+directory = os.path.dirname(os.path.realpath(__file__))
+
+print(directory)
+
+list_files = subprocess.run(['python','-u','train.py',
+                             '--num-cpus','2','--num-gpus','0','--local-dir','./ray_results','--max-episode-steps','250'], cwd=directory)
 
 print("The exit code was: %d" % list_files.returncode)
