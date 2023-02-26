6d4851fc6d91a6510d644ad87a14c3b07d224f98
rakeshwalisheter <rakeshw@berkeley.edu>
2023-02-26 11:47:33 -0800

    debugging

--- examples/marl/rllib/heterogeneous/train_subproc.py
+++ examples/marl/rllib/heterogeneous/train_subproc.py
@@ -5,8 +5,8 @@ directory = os.path.dirname(os.path.realpath(__file__))
 
 print(directory)
 
-list_files = subprocess.run(['python','-u','train_hs.py','--stop-iters','3',
-                             '--num-cpus','2','--num-gpus','0','--local-dir','./ray_results',
+list_files = subprocess.run(['python','-u','train_hs.py','--stop-iters','20',
+                             '--num-cpus','4','--num-gpus','1','--local-dir','./ray_results',
                              '--max-episode-steps','250'],
                             cwd=directory)
 
