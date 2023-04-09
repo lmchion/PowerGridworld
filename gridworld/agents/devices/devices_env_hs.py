fix cost calculation

gridworld/agents/devices/devices_env_hs.py
@@ -161,8 +161,8 @@ class HSDevicesEnv(ComponentEnv):
         step_cost = self.current_cost * self._real_power * (self.minutes_per_step/60.0)
 
         step_meta = {}
-
-        reward = -np.exp(2*step_cost)
+        reward = - np.exp(step_cost)
+        #reward = -(1+reward)**3
 
         # # This is the final sub-environment which gets to act in the system. On each step,
         # # if there is any solar or battery juice left which does not get used, penalize this.

gridworld/agents/energy_storage/energy_storage_env_hs.py
@@ -172,8 +172,8 @@ class HSEnergyStorageEnv(ComponentEnv):
             step_cost = self.delta_cost * self._real_power * self.control_interval_in_hr
 
         #the reward has to be negative so higher reward for less cost
-
-        reward = -np.exp(step_cost)
+        reward = - np.exp(step_cost)
+        #reward = -(1+reward)**3
 
         # solar_capacity = kwargs['pv_power']
         # battery_capacity = kwargs['es_power']

gridworld/agents/vehicles/ev_charging_env_hs.py
@@ -185,7 +185,8 @@ class HSEVChargingEnv(ComponentEnv):
 
         step_meta = {}
 
-        reward = -np.exp((step_cost + kwargs['max_grid_cost'] * self.state["real_power_unserved"]))
+        reward = -np.exp(step_cost+kwargs['max_grid_cost'] * self.state["real_power_unserved"])
+        #reward = -(1+ reward)**3
         
         step_meta["device_id"] = self.name
         step_meta["timestamp"] = kwargs['timestamp']

gridworld/base_hs.py
@@ -214,7 +214,7 @@ class HSMultiComponentEnv(MultiComponentEnv):
         ##### PV and Battery optimization #####
 
         mult_unused_power=25.0
-        mult_unused_storage=1
+        mult_unused_storage=1.0
 
         es_env = [e for e in self.envs if e.name=='storage']
         es_max_storage = max(es_env[0].storage_range)
@@ -238,7 +238,10 @@ class HSMultiComponentEnv(MultiComponentEnv):
             if kwargs['es_current_storage'] > es_min_storage:
                 unused_es_storage= (kwargs['es_current_storage'] - es_min_storage) * (60/self.minutes_per_step)
                 #reward -= mult_unused_storage*unused_es_storage * kwargs['max_grid_cost'] * (self.minutes_per_step/60.0)
-                reward -= mult_unused_power* ( self.unused_power ) * kwargs['max_grid_cost'] * (self.minutes_per_step/60.0)        
+            reward -= mult_unused_power* ( self.unused_power ) * kwargs['max_grid_cost'] * (self.minutes_per_step/60.0) 
+            #reward -= (1+reward)**3        
+        #reward -= np.exp( ( self.unused_power ) * kwargs['max_grid_cost'] * (self.minutes_per_step/60.0)  )      
+
 
         if False:
         